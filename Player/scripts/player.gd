a08c360c02d686f242108d894e927611cb0b9b11
ahmed1212safwat-jpg <236269579+ahmdsfwt257-cloud@users.noreply.github.com>
2026-05-11 15:00:59 +0300

    some fixes in scripts and i add an audio to sword attack, created HitBox, plant prop and other stuff
    fix some bugs like when player moves diagonal the velocity looks faster than when the player moves in a straight line and i added camera 2 D to track the player and clear the player script. i improved the animation and added an attack animation, shadow, attack effect and attack script, issues with attack effect and fixes, added audio to attack, added attack movement deceleration, created a HitBox scene, created HurtBox, created a Plant Prop, build a plant, HitBox, HurtBox script added some plants to playground level, fix collision and sorting issues with plant, i made HurtBox only Active during attack , used an await timer and i make HurtBox rotate with player direction

--- Player/scripts/player.gd
+++ Player/scripts/player.gd
@@ -7,7 +7,7 @@ var direction : Vector2 = Vector2.ZERO
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var state_machine: PlayerStateMachine = $StateMachine
 
-
+signal DirectionChanged( new_direction : Vector2 )
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,25 +17,19 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process( _delta ):
-	
-	
-	#direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	#direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	
 	direction = Vector2(
 		Input.get_axis("left", "right"),
 		Input.get_axis("up", "down")
 	).normalized()
 	pass
 
+
 func _physics_process( _delta ):
 	move_and_slide()
 
 
 
-
 func setDirection() -> bool:
-	
 	var new_dir : Vector2 = cardinal_direction
 	if direction == Vector2.ZERO:
 		return false
@@ -49,6 +43,7 @@ func setDirection() -> bool:
 		return false
 	
 	cardinal_direction = new_dir
+	DirectionChanged.emit( new_dir )
 	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
 	return true
 
--- Player/scripts/state_attack.gd
+++ Player/scripts/state_attack.gd
@@ -2,16 +2,33 @@ class_name State_Attack  extends State
 
 var attacking : bool = false
 
-@onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
-@onready var idle: State_Idle = $"../idle"
+@export var attack_sound : AudioStream
+@export_range(1,20,0.5) var decelerate_speed : float = 5.0
 
+@onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
+@onready var attack_anim: AnimationPlayer = $"../../Sprite2D/AttackEffectSprite/AnimationPlayer"
+@onready var audio: AudioStreamPlayer2D = $"../../Audio/AudioStreamPlayer2D"
+
+@onready var idle: State_Idle = $"../idle"
 @onready var walk: State_walk = $"../walk"
+@onready var hurt_box: HurtBox = $"../../Interactions/HurtBox"
+
+
 
 ## what happens when the player enters this state?
 func Enter() -> void:
 	player.UpdateAnimation("attack")
+	attack_anim.play("attack_" + player.AnimDirection())
 	animation_player.animation_finished.connect(EndAttack)
+	
+	audio.stream = attack_sound
+	audio.pitch_scale = randf_range( 0.9, 1.1 )
+	audio.play()
+	
 	attacking = true 
+	
+	await get_tree().create_timer(0.075).timeout
+	hurt_box.monitoring = true
 	pass
 
 
@@ -19,12 +36,13 @@ func Enter() -> void:
 func Exit() -> void:
 	animation_player.animation_finished.disconnect(EndAttack)
 	attacking = false
+	hurt_box.monitoring = false
 	pass
 
 
 ##what happens during the _process update in this state?
 func process(_delta: float) -> State:
-	player.velocity = Vector2.ZERO
+	player.velocity -= player.velocity * decelerate_speed * _delta 
 	
 	if attacking == false:
 		if player.direction == Vector2.ZERO:
