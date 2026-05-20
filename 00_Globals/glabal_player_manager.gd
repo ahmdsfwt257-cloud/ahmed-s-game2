076dca5dd4fe532f0be74c90424dfb9101a001f6
ahmed1212safwat-jpg <236269579+ahmdsfwt257-cloud@users.noreply.github.com>
2026-05-20 23:37:17 +0300

    i cleaned the code and create a interaction system to player, treasue chest.
    update: i added to our resource based inventory system by adding item pickups and a resource based item effect system for using items. at first i saved Functions in InventoryData Resource, Update Save Manage, Load Functionality. update: create a basic interaction system for our player, and add a Treasure Chest scene that will work with our resource based inventory system. At first i started with adding interactions area then i add interaction input and signal, build a treasure chest scene , Animations and it's script.

--- 00_Globals/glabal_player_manager.gd
+++ 00_Globals/glabal_player_manager.gd
@@ -3,6 +3,8 @@ extends Node
 const PLAYER = preload("uid://c0x18s70vbkvh")
 const INVENTORY_DATA : InventoryData = preload("res://GUI/pause_menu/inventory/player_inventory.tres")
 
+signal interact_pressed
+
 var player : Player
 var player_spawned : bool = false
 
--- Enemies/Scripts/states/enemy_state_destroy.gd
+++ Enemies/Scripts/states/enemy_state_destroy.gd
@@ -1,5 +1,6 @@
 class_name EnemyStateDestroy extends EnemyState
 
+const  PICKUP = preload( "res://Items/item_pickup/item_pickup.tscn" )
 
 @export var anim_name : String = "destroy"
 @export var knockback_speed : float = 200.0
@@ -7,8 +8,13 @@ class_name EnemyStateDestroy extends EnemyState
 
 @export_category("AI")
 
+@export_category( " Item Drops " )
+@export var drops : Array[ DropData ]
+
 var _damage_position : Vector2
 var _direction : Vector2
+
+
 
 
 ##what happens when initialize this statea
@@ -25,6 +31,8 @@ func enter() -> void:
 	enemy.velocity = _direction * -knockback_speed
 	enemy.update_animation( anim_name )
 	enemy.animation_player.animation_finished.connect( _on_animation_finished )
+	disable_hurt_box()
+	drop_items()
 	pass
 
 
@@ -38,9 +46,11 @@ func process( _delta: float) -> EnemyState:
 	enemy.velocity -= enemy.velocity * decelerate_speed * _delta
 	return null
 
+
 ## what happens durnig _physics_process update in this state?
 func physics(_delta: float) -> EnemyState:
 	return null
+
 
 func _on_enemy_destroyed( hurt_box : HurtBox ) -> void:
 	_damage_position = hurt_box.global_position
@@ -49,3 +59,27 @@ func _on_enemy_destroyed( hurt_box : HurtBox ) -> void:
 
 func _on_animation_finished( _a : String ) -> void:
 	enemy.queue_free()
+
+
+func disable_hurt_box() -> void:
+	var hurt_box : HurtBox = enemy.get_node_or_null( " HurtBox " )
+	if hurt_box:
+		hurt_box.monitoring = false
+
+
+
+func drop_items() -> void:
+	if drops.size() == 0:
+		return
+	
+	for i in drops.size():
+		if drops[ i ] == null or drops[ i ].item == null:
+			continue
+		var drop_count : int = drops[ i ].get_drop_count()
+		for j in drop_count:
+			var drop : ItemPickup = PICKUP.instantiate() as ItemPickup
+			drop.item_data = drops[ i ] .item
+			enemy.get_parent().call_deferred( "add_child", drop )
+			drop.global_position = enemy.global_position
+			drop.velocity = enemy.velocity.rotated( randf_range( -1.5, 1.5 ) ) * randf_range( 0.9 , 1.5 )
+	pass
