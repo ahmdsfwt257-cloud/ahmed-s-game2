97921adceb193b910fdad271c658923cf54daeb6
ahmed1212safwat-jpg <236269579+ahmdsfwt257-cloud@users.noreply.github.com>
2026-06-15 05:09:47 +0300

    cleaning up my code and fixing bugs.
    i was decided to focus on cleaning up and fixing bugs in the NPC system today. I added some small pauses in between commas and periods, and I tweaked the pitch of the NPC's voice to slightly lower at the end of sentences to make the dialogue text more natural. Also corrected the glitch that made wandering NPCs walk or turn when I was interacting with them. My biggest rabbit hole was the frustrating problem with my NPCs I was having with patrolling. Once it spoke to one, it would instantaneously find its next victim! For a long time I've been wondering about this. I'm not sure what I did to the problem, but my start() was wiping out their routine. In order to solve this problem, I had to re-implement the logic, separate it into two pieces, idle_phase and walk_phase, and add a has_started flag to remember exactly what the NPC was doing before the chat. All is now well!

--- npc/scripts/npc_behavior_patrol.gd
+++ npc/scripts/npc_behavior_patrol.gd
@@ -9,6 +9,13 @@ var patrol_locations : Array[ PatrolLocation ]
 var current_location_index : int = 0
 var target : PatrolLocation
 
+var has_started : bool = false
+var last_phase : String = ""
+var direction : Vector2
+
+@onready var timer: Timer = $Timer
+
+
 
 
 func _ready() -> void:
@@ -29,7 +36,7 @@ func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		return
 	if npc.global_position.distance_to( target.target_position ) < 1:
-		start()
+		idle_phase()
 
 
 
@@ -56,7 +63,6 @@ func gather_patrol_locations( _n : Node = null ) -> void:
 				else:
 					_next = patrol_locations[ 0 ]
 				_p.update_line( _next.position )
-				
 	pass
 
 
@@ -65,6 +71,17 @@ func start() -> void:
 	if npc.do_behavior == false or patrol_locations.size() < 2:
 		return
 	
+	if has_started == true:
+		if timer.time_left == 0:
+			walk_phase()
+		return #idle phase is still waiting for the timer timeout
+	
+	has_started = true
+	idle_phase()
+	pass
+
+
+func idle_phase() -> void:
 	#IDLE PHASE
 	npc.global_position = target.target_position
 	npc.state = "idle"
@@ -79,15 +96,23 @@ func start() -> void:
 	
 	target = patrol_locations[ current_location_index ]
 	
-	await get_tree().create_timer( wait_time ).timeout
+	if wait_time > 0:
+		timer.start(wait_time)
+		await timer.timeout
 	
 	if npc.do_behavior == false:
 		return
 	
+	walk_phase()
+	pass
+
+
+
+func walk_phase() -> void:
 	npc.state = "walk"
-	var _dir = global_position.direction_to( target.target_position )
-	npc.direction = _dir
-	npc.velocity = walk_speed * _dir
+	direction = global_position.direction_to( target.target_position )
+	npc.direction = direction
+	npc.velocity = walk_speed * direction
 	npc.update_direction( target.target_position )
 	npc.update_animation()
 	pass
